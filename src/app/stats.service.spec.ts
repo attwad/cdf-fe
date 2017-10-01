46c8d0c56f2bca91bb77825298eed5e978f41b29
tmu <attwad@users.noreply.github.com>
2017-10-01 17:14:05 +0900

    added tests for stats service

--- src/app/stats.service.spec.ts
+++ src/app/stats.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed, inject } from '@angular/core/testing';
-import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
-import { StatsService } from './stats.service';
+import { StatsService, Stats } from './stats.service';
 
 describe('StatsService', () => {
   beforeEach(() => {
@@ -13,5 +13,17 @@ describe('StatsService', () => {
 
   it('should be created', inject([StatsService], (service: StatsService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('should fetch the stats', inject([StatsService, HttpTestingController], (service: StatsService, httpMock: HttpTestingController) => {
+    service.getStats().subscribe((stats: Stats) => {
+      expect(stats.NumConverted).toEqual(42);
+    });
+    const req = httpMock.expectOne('/api/stats');
+    expect(req.request.method).toEqual('GET');
+    req.flush({
+      NumConverted: 42
+    });
+    httpMock.verify();
   }));
 });
